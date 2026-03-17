88105f6ddd4b4ebda29f803ad0ed46d43d1dd2da
Youness AitJamaa <aitjamaayouness1@gmail.com>
2026-03-17 10:57:14 +0000

    Add a Middleware For roles

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $role): Response
+    {
+        if(!auth('api')->check()) {
+            return response()->json([
+                'message' => 'Unauthenticated'
+            ], 401);
+        }
+
+        if(auth('api')->user()->role !== $role) {
+            return response()->json([
+                'message' => 'Access Denied'
+            ], 403);
+        }
+        return $next($request);
+    }
+}
