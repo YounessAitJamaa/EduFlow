f5d8971f4603c1de4894a02596fe1dde2d0bdd56
Youness AitJamaa <aitjamaayouness1@gmail.com>
2026-03-17 11:06:08 +0000

    Add table courses and Relation to users tabel

---
+++ app/Models/Course.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'teacher_id',
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class, 'teacher_id');
+    }
 }
